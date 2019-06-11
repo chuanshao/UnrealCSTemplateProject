18382f8b45f1528691103387e6c30a8458e80f0a
chuanshao <chuanshaooye@163.com>
2019-06-12 01:39:49 +0800

    add Unreal C# Item Template for USharp

---
+++ UnrealActorComponentTemplate/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnrealEngine.Engine;
+using UnrealEngine.InputCore;
+using UnrealEngine.Runtime;
+
+[UClass]
+public class $safeitemrootname$ : UActorComponent
+{
+    public override void Initialize(FObjectInitializer initializer)
+    {
+    base.Initialize(initializer);
+    // Open Tick
+    //PrimaryActorTick.SetStartWithTickEnabled(true);
+    //PrimaryActorTick.SetCanEverTick(true);
+    }
+
+    public override void BeginPlay()
+    {
+        base.BeginPlay();
+    }
+
+    //Receive Tick Fun
+    //protected override void ReceiveTick_Implementation(float DeltaSeconds)
+    //{
+    //    base.ReceiveTick_Implementation(DeltaSeconds);
+    //}
+
+}
---
+++ UnrealActorTemplate/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnrealEngine.Engine;
+using UnrealEngine.InputCore;
+using UnrealEngine.Runtime;
+
+[UClass]
+public class $safeitemrootname$ : AActor
+{
+    public override void Initialize(FObjectInitializer initializer)
+    {
+    base.Initialize(initializer);
+    // Open Tick
+    //PrimaryActorTick.SetStartWithTickEnabled(true);
+    //PrimaryActorTick.SetCanEverTick(true);
+    }
+
+    protected override void BeginPlay()
+    {
+        base.BeginPlay();
+    }
+
+    //Receive Tick Fun
+    //protected override void ReceiveTick_Implementation(float DeltaSeconds)
+    //{
+    //    base.ReceiveTick_Implementation(DeltaSeconds);
+    //}
+
+}
